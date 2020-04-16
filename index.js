cc731173e7d47d8fa1d90eecd912b531b1c19be7
Antonjtdnhf <antondelaushiy@gmail.com>
2020-04-16 18:35:57 +0300

    created one line battle sea game

---
+++ index.js
@@ -0,0 +1,223 @@
+let loc1 = 2;
+let loc2 = 3;
+let loc3 = 4;
+let isSunk = false;
+let guesses = 0;
+let hits = 0;
+
+while (isSunk === false) {
+    let guess = +prompt("", "2");
+    if (guess === loc1) {
+        alert(" You've hit the ship!");
+        guesses++;
+        hits++;
+        while (isSunk === false) {
+            let guess = +prompt("", "2");
+            if (guess === loc1) {
+                alert(" Already has been hit!");
+                guesses++;
+            } else if (guess === loc2) {
+                alert("You've hit the ship twice!");
+                hits++;
+                guesses++;
+                while (isSunk === false) {
+                    let guess = +prompt("", "2");
+                    if (guess === loc2 || guess === loc1) {
+                        alert(" Already has been hit!");
+                        guesses++;
+                    } else if (guess === loc3) {
+                        alert("You've sunk a ship!");
+                        hits++;
+                        guesses++;
+                        isSunk = true;
+                    } else {
+                        alert("You've missed!");
+                        guesses++;
+                    }
+                }
+            } else if (guess === loc3) {
+                alert("You've hit the ship twice!");
+                hits++;
+                guesses++;
+                while (isSunk === false) {
+                    let guess = +prompt("", "2");
+                    if (guess === loc3 || guess === loc1) {
+                        alert(" Already has been hit!");
+                        guesses++;
+                    } else if (guess === loc2) {
+                        alert("You've sunk a ship!");
+                        hits++;
+                        guesses++;
+                        isSunk = true;
+                    } else {
+                        alert("You've missed!");
+                        guesses++;
+                    }
+                }
+            } else {
+                alert("You've missed!");
+                guesses++;
+            }
+        }
+    } else if (guess === loc2) {
+        alert(" You've hit the ship!");
+        guesses++;
+        hits++;
+        while (isSunk === false) {
+            let guess = +prompt("", "2");
+            if (guess === loc2) {
+                alert(" Already has been hit!");
+                guesses++;
+            } else if (guess === loc1) {
+                alert("You've hit the ship twice!");
+                hits++;
+                guesses++;
+                while (isSunk === false) {
+                    let guess = +prompt("", "2");
+                    if (guess === loc2 || guess === loc1) {
+                        alert(" Already has been hit!");
+                        guesses++;
+                    } else if (guess === loc3) {
+                        alert("You've sunk a ship!");
+                        hits++;
+                        guesses++;
+                        isSunk = true;
+                    } else {
+                        alert("You've missed!");
+                        guesses++;
+                    }
+                }
+            } else if (guess === loc3) {
+                alert("You've hit the ship twice!");
+                hits++;
+                guesses++;
+                while (isSunk === false) {
+                    let guess = +prompt("", "2");
+                    if (guess === loc3 || guess === loc2) {
+                        alert(" Already has been hit!");
+                        guesses++;
+                    } else if (guess === loc1) {
+                        alert("You've sunk a ship!");
+                        hits++;
+                        guesses++;
+                        isSunk = true;
+                    } else {
+                        alert("You've missed!");
+                        guesses++;
+                    }
+                }
+            } else {
+                alert("You've missed!");
+                guesses++;
+            }
+        }
+    } else if (guess === loc2) {
+        alert(" You've hit the ship!");
+        guesses++;
+        hits++;
+        while (isSunk === false) {
+            let guess = +prompt("", "2");
+            if (guess === loc2) {
+                alert(" Already has been hit!");
+                guesses++;
+            } else if (guess === loc1) {
+                alert("You've hit the ship twice!");
+                hits++;
+                guesses++;
+                while (isSunk === false) {
+                    let guess = +prompt("", "2");
+                    if (guess === loc2 || guess === loc1) {
+                        alert(" Already has been hit!");
+                        guesses++;
+                    } else if (guess === loc3) {
+                        alert("You've sunk a ship!");
+                        hits++;
+                        guesses++;
+                        isSunk = true;
+                    } else {
+                        alert("You've missed!");
+                        guesses++;
+                    }
+                }
+            } else if (guess === loc3) {
+                alert("You've hit the ship twice!");
+                hits++;
+                guesses++;
+                while (isSunk === false) {
+                    let guess = +prompt("", "2");
+                    if (guess === loc3 || guess === loc2) {
+                        alert(" Already has been hit!");
+                        guesses++;
+                    } else if (guess === loc1) {
+                        alert("You've sunk a ship!");
+                        hits++;
+                        guesses++;
+                        isSunk = true;
+                    } else {
+                        alert("You've missed!");
+                        guesses++;
+                    }
+                }
+            } else {
+                alert("You've missed!");
+                guesses++;
+            }
+        }
+    } else if (guess === loc3) {
+        alert(" You've hit the ship!");
+        guesses++;
+        hits++;
+        while (isSunk === false) {
+            let guess = +prompt("", "2");
+            if (guess === loc3) {
+                alert(" Already has been hit!");
+                guesses++;
+            } else if (guess === loc1) {
+                alert("You've hit the ship twice!");
+                hits++;
+                guesses++;
+                while (isSunk === false) {
+                    let guess = +prompt("", "2");
+                    if (guess === loc3 || guess === loc1) {
+                        alert(" Already has been hit!");
+                        guesses++;
+                    } else if (guess === loc2) {
+                        alert("You've sunk a ship!");
+                        hits++;
+                        guesses++;
+                        isSunk = true;
+                    } else {
+                        alert("You've missed!");
+                        guesses++;
+                    }
+                }
+            } else if (guess === loc2) {
+                alert("You've hit the ship twice!");
+                hits++;
+                guesses++;
+                while (isSunk === false) {
+                    let guess = +prompt("", "2");
+                    if (guess === loc3 || guess === loc2) {
+                        alert(" Already has been hit!");
+                        guesses++;
+                    } else if (guess === loc1) {
+                        alert("You've sunk a ship!");
+                        hits++;
+                        guesses++;
+                        isSunk = true;
+                    } else {
+                        alert("You've missed!");
+                        guesses++;
+                    }
+                }
+            } else {
+                alert("You've missed!");
+                guesses++;
+            }
+        }
+    } else {
+        alert("You've missed!");
+        guesses++;
+    }
+}
+alert("You've won!");
